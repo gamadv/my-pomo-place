d0266b024a3d9a3c923f04a1ed5d7b3c3a83729c
gamadv <lealdev.py@gmail.com>
2022-08-11 17:22:45 -0300

    feat: global styles and theme started

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-  return <div className="App">hi</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div className="App">hi</div>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
