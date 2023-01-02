5d6177046918996b942e06ce493fadab319346bc
gamadv <lealdev.py@gmail.com>
2023-01-01 21:59:58 -0300

    feat: Insert new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div className="App">hi</div>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
