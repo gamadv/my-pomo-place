3039e3044e0ebc459755d6e9e9a10aaa3760b12d
gamadv <lealdev.py@gmail.com>
2023-01-01 21:59:11 -0300

    chore: Extends Styled components defaulttheme

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,8 +1,8 @@
-import "styled-components";
-import { defaultTheme } from "../styles/themes/default";
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
-type Themetype = typeof defaultTheme;
+type Themetype = typeof defaultTheme
 
-declare module "styled-components" {
-    export interface DefaultTheme extends Themetype {}
+declare module 'styled-components' {
+  export interface DefaultTheme extends Themetype {}
 }
